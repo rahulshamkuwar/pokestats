d2a0f78b3077c18154aabc9486f2fa13c5d38d56
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-01-18 21:32:25 -0700

    Initial layout with data shown

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="container mx-auto m-10">
+      <h1 className="text-3xl">PokéStats</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-roboto)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
